a409e66158f6c528aabf17e835f15160d64e7405
magicmannumber1 <l.blair@easygo.io>
2025-11-11 16:11:20 +1100

    Update Svelte config for production paths and adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // Static site generation (SSG) is used: https://kit.svelte.dev/docs/adapter-static
     adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: undefined,
+      precompress: false,
       strict: false,
     }),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/free-plinko-game-online' : '',
+    }
   },
 };
-
-export default config;
