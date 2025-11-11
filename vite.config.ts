30c3a2dc52b2baa7a778f32f130de49aa9d404d4
magicmannumber1 <l.blair@easygo.io>
2025-11-11 16:13:56 +1100

    Update Vite config to include Vitest settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
   base: '/free-plinko-game-online/',
   plugins: [sveltekit()],
